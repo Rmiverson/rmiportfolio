c1e43d0c6d6fc74224de91419d082b9ee9329d4d
Rmiverson <Rmiverson99@gmail.com>
2021-05-12 21:07:02 -0700

    added about home more projects and notfound js files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { Switch } from 'react-router';
 import './scss/App.scss';
 
+import NotFound from './components/NotFound'
+import Home from './containers/Home'
+import About from './containers/About'
+import More from './containers/More'
+import Projects from './containers/Projects'
+
 class App extends React.Component {
   render () {
     return (
       <div className="App">
         <Switch>
-        
+          <Route exact path="/home" render={() => <Home />} />
+          <Route exact path="/home" render={() => <About />} />
+          <Route exact path="/home" render={() => <Projects />} />
+          <Route exact path="/home" render={() => <More />} />
+
+          <Route component={NotFound} />
         </Switch>
       </div>
     )
---
+++ src/containers/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+   return (
+      <div className="about-page">
+         <h2>about page</h2>
+      </div>
+   )
+}
+
+export default About
