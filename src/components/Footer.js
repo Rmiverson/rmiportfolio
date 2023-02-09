b728d480e332336a18af0a15babcdcaefc030e8a
Riley Iverson <rmiverson99@gmail.com>
2023-02-08 19:47:36 -0800

    created a nav component and a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; Riley Iverson 2023</p>
+        </footer>
+    )
+}
+
+export default Footer
