b1b25f84fa76d4ed9b0140cc45afa77f63278d6c
rmiverson <rmiverson99@gmail.com>
2023-02-10 12:03:06 -0800

    created a splash for the landing page

---
+++ src/components/Splash.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Splash = () => {
+    return (
+        <div className="splash">
+            <h1>Riley</h1>
+            <h1>Iverson</h1>
+            <hr/>
+            <h3>Software Developer</h3>
+            <h5>As well as other things.</h5>
+        </div>
+    )
+}
+
+export default Splash
