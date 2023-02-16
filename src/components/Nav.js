907ec47bcb4b5b0644d8ac9267043dbb1fe0c2c6
rmiverson <rmiverson99@gmail.com>
2023-02-16 10:29:37 -0800

    styled the navbar with a neumorphic style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className="navbar">
+        <nav className="nav">
             <Link className="nav-item" to='/'>About Me</Link>
             <Link className="nav-item" to='/development'>Development</Link>
             <Link className="nav-item" to='/design'>Design</Link>
