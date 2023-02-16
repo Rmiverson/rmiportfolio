6570ba4407fc700c0d72ce619994b7b7fc4aae3b
rmiverson <rmiverson99@gmail.com>
2023-02-16 11:35:59 -0800

    removed alt options because thats not a thing for anchor tags

--- src/components/SocialBar.js
+++ src/components/SocialBar.js
@@ -5,11 +5,11 @@ import { BsLinkedin, BsTwitter, BsInstagram, BsMedium, BsDribbble } from 'react-
 const SocialBar = () => {
     return (
         <div className="social-bar">
-            <a alt="Linkedin" href="https://www.linkedin.com/in/rmiverson/"><BsLinkedin />Linkedin</a>
-            <a alt="Twitter" href="https://twitter.com/rmiverson99"><BsTwitter />Twitter</a>
-            <a alt="Instagram" href="https://www.instagram.com/rmiverson/"><BsInstagram />Instagram</a>
-            <a alt="Medium" href="https://rmiverson.medium.com/"><BsMedium />Medium</a>
-            <a alt="Dribble" href="https://dribbble.com/rmiverson"><BsDribbble />Dribble</a>
+            <a href="https://www.linkedin.com/in/rmiverson/"><BsLinkedin />Linkedin</a>
+            <a href="https://twitter.com/rmiverson99"><BsTwitter />Twitter</a>
+            <a href="https://www.instagram.com/rmiverson/"><BsInstagram />Instagram</a>
+            <a href="https://rmiverson.medium.com/"><BsMedium />Medium</a>
+            <a href="https://dribbble.com/rmiverson"><BsDribbble />Dribble</a>
         </div>
     )
 }
