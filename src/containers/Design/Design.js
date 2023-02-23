acdfcc0d533184f1340f54d5ac8288bd2ee7f000
rmiverson <rmiverson99@gmail.com>
2023-02-23 10:50:01 -0800

    set up for design page

--- src/containers/Design/Design.js
+++ src/containers/Design/Design.js
@@ -1,12 +1,67 @@
 import React from "react";
+import Footer from "../../components/Footer";
 import Nav from "../../components/Nav";
 
+import './Design.scss'
+
 const Design = () => {
-    return (
-        <div className="design">
-            <Nav />
+return (
+    <div className="design">
+      <div className="mid-ribbon">
+        <div className="inner-mid-ribbon"></div>
+      </div>
+      <Nav />
+
+      <div className="design-content min-height">
+        <div className="design-introduction">
+          <h1>Design & Creative</h1>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
-    );
+
+        <div className="design-skills-list">
+          <div className="design-skills-container">
+            <h3>Skills</h3>
+
+            <ul>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+            </ul>
+          </div>
+
+          <div className="design-software-container">
+            <h3>Software</h3>
+
+            <ul>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+              <li>test</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="design-projects">
+
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Design;
