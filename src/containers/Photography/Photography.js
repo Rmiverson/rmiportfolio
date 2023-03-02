381b21dc199bd5f0704f6c324ee929c1a56fa0d6
rmiverson <rmiverson99@gmail.com>
2023-03-02 11:07:29 -0800

    added base for photo page

--- src/containers/Photography/Photography.js
+++ src/containers/Photography/Photography.js
@@ -1,13 +1,97 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Footer from '../../components/Footer'
 import Nav from '../../components/Nav'
+
 import './Photography.scss'
 
 const Photography = () => {
-return (
-  <div className='photography'>
-    <Nav />
-  </div>
-  )
+  const [result, setResult] = useState({data: {}, status: null, message: null})
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        await fetch('db.json', {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          setResult({data: data, status: 200, message: 'OK'})
+        })
+      } catch (error) {
+        setResult({data: {}, status: error, message: error})
+      }
+    }
+  
+    getData()
+  }, [])
+
+  if (!result.status) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className='photography'>
+        <div className='mid-ribbon'>
+          <div className='inner-mid-ribbon'></div>
+        </div>
+
+        <Nav />
+
+        <div className='photography-content min-height'>
+          <div className='photography-introduction'>
+            <h1>Photography</h1>
+            <p>{result.data.photography.about}</p>
+          </div>
+
+          <div className='skills-list'>
+            <div className='skills-container'>
+              <h3>Skills:</h3>
+              <ul>
+                {result.data.photography.skills.map((skill, index) => {
+                  let returnValue = skill
+
+                  if (index + 1 !== result.data.photography.skills.length) {
+                    returnValue = `${skill},`
+                  }
+
+                  return (
+                    <li key={skill + index}>{returnValue}</li>
+                  )
+                })}
+              </ul>
+            </div>
+
+            <div className='software-container'>
+              <h3>Software:</h3>
+              <ul>
+                {result.data.photography.software.map((software, index) => {
+                  let returnValue = software
+
+                  if (index + 1 !== result.data.photography.software.length) {
+                    returnValue = `${software},`
+                  }
+
+                  return (
+                    <li key={software + index}>{returnValue}</li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+
+          <div className='photography examples'>
+            {result.data.photography.media.map((media, index) => {
+              return <img alt='photography example' src={`/media/photography/${media}`}/>
+            })}
+          </div>
+
+          <Footer />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Photography
