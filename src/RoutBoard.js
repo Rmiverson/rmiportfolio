07ea0b0f0e9398777d44cd4c8b48f619374e32be
rmiverson <rmiverson99@gmail.com>
2023-03-03 11:41:47 -0800

    prepped for deployment, adding custion favicon removing comments and redoing the readme

--- src/RoutBoard.js
+++ src/RoutBoard.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import AboutMe from "./containers/AboutMe/AboutMe.js"
-import Contact from "./containers/Contact/Contact.js"
-import Design from "./containers/Design/Design.js"
-import Development from "./containers/Development/Development.js"
-import Photography from "./containers/Photography/Photography.js"
+import AboutMe from './containers/AboutMe/AboutMe.js'
+import Contact from './containers/Contact/Contact.js'
+import Design from './containers/Design/Design.js'
+import Development from './containers/Development/Development.js'
+import Photography from './containers/Photography/Photography.js'
 
 const RouteBoard = () => {
     return (
         <Routes>
             <Route index element={<AboutMe />} />
-            <Route path="/development" element={<Development />} />
-            <Route path="/design" element={<Design />} />
-            <Route path="/photography" element={<Photography />} />
-            <Route path="/Contact" element={<Contact />} />
+            <Route path='/development' element={<Development />} />
+            <Route path='/design' element={<Design />} />
+            <Route path='/photography' element={<Photography />} />
+            <Route path='/Contact' element={<Contact />} />
 
-            <Route path="*" element={<div>404 Page Not Found.</div>} />
+            <Route path='*' element={<div>404 Page Not Found.</div>} />
         </Routes>
     )
 }
