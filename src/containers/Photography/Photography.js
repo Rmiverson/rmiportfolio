4e9d14b869e032381181b06e24ffe57d534a608e
rmiverson <rmiverson99@gmail.com>
2023-03-04 19:09:13 -0800

    updated photography page

--- src/containers/Photography/Photography.js
+++ src/containers/Photography/Photography.js
@@ -1,34 +1,10 @@
-import React, {useState, useEffect} from 'react'
-import Footer from '../../components/Footer'
-import Nav from '../../components/Nav'
-import {AiOutlineClose} from 'react-icons/ai'
+import React, { useState } from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
 
 import './Photography.scss'
 
-const Photography = () => {
-  const [result, setResult] = useState({data: {}, status: null, message: null})
+const Photography = (props) => {
   const [modal, setModal] = useState({display: 'hidden', media: null})
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        await fetch('db.json', {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          }
-        })
-        .then((response) => response.json())
-        .then((data) => {
-          setResult({data: data, status: 200, message: 'OK'})
-        })
-      } catch (error) {
-        setResult({data: {}, status: error, message: error})
-      }
-    }
-  
-    getData()
-  }, [])
 
   const handleClick = (e) => {
     e.preventDefault()
@@ -42,75 +18,61 @@ const Photography = () => {
     setModal({display: 'hidden', media: null})
   }
 
-  if (!result.status) {
-    return <div>Loading...</div>
-  } else {
-    return (
-      <div className='photography'>
-        <div className='mid-ribbon'>
-          <div className='inner-mid-ribbon'></div>
+  return (
+    <div className='photography'>
+        <div className='photography-introduction'>
+          <h1>Photography</h1>
+          <p>{props.data.photography.about}</p>
         </div>
 
-        <Nav />
+        <div className='photography-skills-list'>
+          <div className='photography-skills-container'>
+            <h3>Skills:</h3>
+            <ul>
+              {props.data.photography.skills.map((skill, index) => {
+                let returnValue = skill
 
-        <div className='photography-content min-height'>
-          <div className='photography-introduction'>
-            <h1>Photography</h1>
-            <p>{result.data.photography.about}</p>
+                if (index + 1 !== props.data.photography.skills.length) {
+                  returnValue = `${skill},`
+                }
+
+                return <li key={skill + index}>{returnValue}</li>
+              })}
+            </ul>
           </div>
 
-          <div className='photography-skills-list'>
-            <div className='photography-skills-container'>
-              <h3>Skills:</h3>
-              <ul>
-                {result.data.photography.skills.map((skill, index) => {
-                  let returnValue = skill
+          <div className='photography-software-container'>
+            <h3>Software:</h3>
+            <ul>
+              {props.data.photography.software.map((software, index) => {
+                let returnValue = software
 
-                  if (index + 1 !== result.data.photography.skills.length) {
-                    returnValue = `${skill},`
-                  }
+                if (index + 1 !== props.data.photography.software.length) {
+                  returnValue = `${software},`
+                }
 
-                  return <li key={skill + index}>{returnValue}</li>
-                })}
-              </ul>
-            </div>
-
-            <div className='photography-software-container'>
-              <h3>Software:</h3>
-              <ul>
-                {result.data.photography.software.map((software, index) => {
-                  let returnValue = software
-
-                  if (index + 1 !== result.data.photography.software.length) {
-                    returnValue = `${software},`
-                  }
-
-                  return <li key={software + index}>{returnValue}</li>
-                })}
-              </ul>
-            </div>
+                return <li key={software + index}>{returnValue}</li>
+              })}
+            </ul>
           </div>
-
-          <div className={modal.display}>
-            <img alt='photography example' src={modal.media}/>
-            <button onClick={handleClose}>{<AiOutlineClose />}</button>
-          </div>
-
-          <div className='photography-examples'>
-            {result.data.photography.media.map((media, index) => {
-              return (
-                <div key={media + index} className='photography-example'>
-                  <img  alt='photography example' src={`/media/photography/${media}`} onClick={handleClick}/>
-                </div>
-              )
-            })}
-          </div>
-
-          <Footer />
         </div>
-      </div>
-    )
-  }
+
+        <div className={modal.display}>
+          <img alt='photography example' src={modal.media}/>
+          <button onClick={handleClose}>{<AiOutlineClose />}</button>
+        </div>
+
+        <div className='photography-examples'>
+          {props.data.photography.media.map((media, index) => {
+            return (
+              <div key={media + index} className='photography-example'>
+                <img  alt='photography example' src={`/media/photography/${media}`} onClick={handleClick}/>
+              </div>
+            )
+          })}
+        </div>
+    </div>
+  )
 }
 
 export default Photography
