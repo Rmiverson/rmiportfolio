fa202bc05d20bdb7313c1bd19fe108bed348a919
rmiverson <rmiverson99@gmail.com>
2026-05-19 11:31:26 -0700

    built out grid system a bit for displaying portfolio pieces

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,22 @@ export default function Work() {
     return (
         <div>
             <h1>Work</h1>
+
+            <div>
+                <h2>Illustrations</h2>
+                <div className="grid grid-cols-3 gap-4">
+                    {/* Grid system for illustrations */}
+                    <div className="flex flex-row gap-4">
+                        {/* Grid item for each illustration */}
+                        <image className="w-40 h-40 bg-olive-500"/>
+                        <div> {/* hidden until image is clicked on */}
+                            <h3>Title</h3>
+                            <p>Summary</p>
+                            <p>Process</p>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
